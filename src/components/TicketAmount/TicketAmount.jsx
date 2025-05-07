42fa337321536598f29e8e084dd01c7f7583d9ca
Emilia Myrup <myrupemilia@gmail.com>
2025-05-07 13:29:20 +0200

    styled components done

--- src/components/TicketAmount/TicketAmount.jsx
+++ src/components/TicketAmount/TicketAmount.jsx
@@ -6,51 +6,64 @@ const TicketWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  width: 310px;
-  height: 96px;
+  width: ${(props) => props.variant === 'detailed' ? '230px' : '310px'};
+  height: ${(props) => props.variant === 'detailed' ? '130px' : '96px'};
   color: #FFFFFF;
   border: 2px solid #F56B9A;
   border-radius: 3px;
   margin: .4rem;
   font-family: 'Fira Sans';
 `
-
 const TicketInfo = styled.div`
   display: flex;
   flex-direction: column;
   border-bottom: 1px solid #F56B9A;
   justify-content: center;
+  align-items: left;
+`
+
+const TicketInfoDetPage = styled.p`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #F56B9A;
+  justify-content: center;
   align-items: center;
+  font-size: 32px;
+  font-weight: 700;
+  text-shadow: 1px 1px 0 #F56B9A;
+  padding: 1rem;
 `
 const TicketSelectAmount = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   height: 100%;
-  gap: 1rem;
+  gap: ${(props) => props.variant === 'detailed' ? '0': '1rem'};
   border-bottom: 1px solid #F56B9A;
 `
 const OrderAddDecreaseButton = styled.button`
-all: unset;
-width: 100px;
-height: 100%;
-text-align: center;
-font-weight: 100;
-font-size: 28px;
-align-items: center;
-justify-content: center;
-cursor: pointer;
+  all: unset;
+  width: 100px;
+  height: 100%;
+  text-align: center;
+  font-weight: 100;
+  font-size: 28px;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `
-
 const TicketQty = styled.p`
   margin: 0;
+  min-width: ${(props) => props.variant === 'detailed' ? '100px' : '125px'};
   line-height: 1;
   font-size: 28px;
   font-weight: 400;
-  background-color: none;
-  padding: 2px 3rem 0 3rem;
+  background-color: transparent;
+  padding: ${(props) => props.variant === 'detailed' ? '2px 2rem 0 2rem' : '2px 3rem 0 3rem'};
   border-left: 1px solid #F56B9A;
   border-right: 1px solid #F56B9A;
+  text-align: ${(props) => props.variant === 'detailed' && 'center'};
+  padding-top: ${(props) => props.variant === 'detailed' && '.7rem'};
 `
 
 function TicketAmount({ info, event, numOfTickets, setNumOfTickets }) {
@@ -74,14 +87,14 @@ function TicketAmount({ info, event, numOfTickets, setNumOfTickets }) {
 
   //Kommer från DetailedEventPage. (`info` inte är en komponent) 
   return (
-    <div className="ticket-amount__wrapper">
-      <p>{info * numOfTickets}</p>
-      <section className='ticket-amount__select-amount'>
-        <button onClick={() => setNumOfTickets (prev => Math.max(prev - 1, 0))}>-</button>
-        <p>{numOfTickets}</p> 
-        <button onClick={() => setNumOfTickets (prev => prev + 1)}>+</button>
-      </section>
-    </div>
+    <TicketWrapper variant='detailed'>
+      <TicketInfoDetPage>{info * numOfTickets}</TicketInfoDetPage>
+      <TicketSelectAmount variant='detailed'>
+        <OrderAddDecreaseButton onClick={() => setNumOfTickets (prev => Math.max(prev - 1, 0))}>-</OrderAddDecreaseButton>
+        <TicketQty variant='detailed'>{numOfTickets}</TicketQty> 
+        <OrderAddDecreaseButton onClick={() => setNumOfTickets (prev => prev + 1)}>+</OrderAddDecreaseButton>
+      </TicketSelectAmount>
+    </TicketWrapper>
   );
 }
 
